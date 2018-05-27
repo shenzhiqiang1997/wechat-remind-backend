8dd28c87e2fe30e592072c35ed0974794c8a8292
no <1422537078@qq.com>
2018-05-27 21:17:51 +0800

    add exception

---
+++ src/main/java/priv/shen/wechat/remind/backend/exception/GlobalException.java
@@ -0,0 +1,7 @@
+package priv.shen.wechat.remind.backend.exception;
+
+public class GlobalException extends RuntimeException{
+    public GlobalException(String message){
+        super(message);
+    }
+}
